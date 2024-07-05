b2f456cd4f13aae8c5e9c98d09b788d827741481
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-05 23:21:52 +0530

    variable types

--- 01_intro/variableMe.ts
+++ 01_intro/variableMe.ts
@@ -14,7 +14,12 @@ let isLoggedIn: boolean = false
 
 
 
-// any
+// we dont need to define type like this : string or boolean or whatevver when we assign something in variable evven if we dont give type by ourself typescript automatically detects its a number or string or whatever datatypes it is.
+
+// dont do this let user: string = "Jack"
+// insted do this let user = "Anas"
+
+// but there are some places where we need to define datayple implicitly that will be covered in future lectures
 
 let hero: string;
 
