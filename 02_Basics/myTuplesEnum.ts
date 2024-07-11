f61c471ad523720120880675f71b575841219b68
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-11 23:08:32 +0530

    tuples in typescript

---
+++ 02_Basics/myTuplesEnum.ts
@@ -0,0 +1,18 @@
+// tuples are not present in javascript it is only present in typescript.
+// its an specialized array with some restriction on to it.
+// not just what type of element in the tuples matters orders of those types also matters  
+
+let Users: [number, string, boolean] = [5, "hello", true]   // tuple
+let rgb: [number, number, number] = [255, 123, 221]
+
+// example with type aliases 
+type User = [number, string]
+const newUser: User = [23, "a@gmail.com"]
+// we can change value also in tuple
+newUser[1] = "abc@gmail.com"
+
+// array methods are also working with tuples but they messes the purpose of tuple.
+newUser.push(5)
+// as we have restricted(guaranteed) that the array of two lenght but still how we can be able to push third element thats the question.
+
+// all array mehtods are working like push, pop, shift, unshift
