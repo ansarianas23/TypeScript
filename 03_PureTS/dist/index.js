fe0d562f7eb113a049211f2655f93e31960ab987
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-14 23:15:53 +0530

    Introduction of TypeScript in production

---
+++ 03_PureTS/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("Hello world");
+const user = {
+    name: "Anas",
+    email: "Anas@gmail.com",
+    age: 20
+};
+console.log(user);
---
+++ 03_PureTS/src/index.ts
@@ -0,0 +1,16 @@
+
+type User = {
+    name: string
+    email: string
+    age: number
+}
+
+console.log("Hello world");
+
+const user: User = {
+    name: "Anas",
+    email: "Anas@gmail.com",
+    age: 20
+}
+
+console.log(user);
