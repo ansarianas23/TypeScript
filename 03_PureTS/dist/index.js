a1e66555de2db038985a3899b7c4a141f6c12fce
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-15 22:58:59 +0530

    protected access modifier

--- 03_PureTS/dist/index.js
+++ 03_PureTS/dist/index.js
@@ -1,9 +1,12 @@
 "use strict";
+// Note:- by default properties are public if not specified 
+// public, private, protected - these words are called as access modifiers
 class User {
     constructor(name, email) {
         this.city = "";
         this.pinCode = 400001;
         this.phoneNo = 123455678;
+        // private _courseCount: number = 1
         this._courseCount = 1;
         this.name = name;
         this.email = email;
@@ -42,4 +45,18 @@ class User {
 const jack = new User("Jack Sharma", "jack@gmail.com");
 jack.city = "mumbai";
 // jack.pinCode = 412330    // restricted as it is read only
-// jack.phoneNo         // not accessible as it is private means it is accessible within class
+// jack.phoneNo         // not accessible as it is private property means it is accessible within class not accessiblen even by class inherited form that class
+// jack.deleteToken()   // not accessible as it is private method means it is accessible within class not accessiblen even by class inherited form that class
+// ----------------------------------------------------------------------------------------------------------------------------
+// Inheritance
+class subUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+// protected properties are accessible within the class as well as class which is inherited from it
+// but is still not by the object that created from that class thats the little deference in between protected and private
--- 03_PureTS/src/index.ts
+++ 03_PureTS/src/index.ts
@@ -1,10 +1,14 @@
+// Note:- by default properties are public if not specified 
+// public, private, protected - these words are called as access modifiers
+
 class User{
     name :string
     email: string
     city: string = ""
     readonly pinCode: number = 400001
     private phoneNo: number = 123455678
-    private _courseCount: number = 1
+    // private _courseCount: number = 1
+    protected _courseCount: number = 1
 
     constructor(name: string, email: string){
         this.name = name
@@ -51,4 +55,19 @@ class User{
 const jack = new User("Jack Sharma", "jack@gmail.com");
 jack.city = "mumbai"
 // jack.pinCode = 412330    // restricted as it is read only
-// jack.phoneNo         // not accessible as it is private means it is accessible within class
+// jack.phoneNo         // not accessible as it is private property means it is accessible within class not accessiblen even by class inherited form that class
+// jack.deleteToken()   // not accessible as it is private method means it is accessible within class not accessiblen even by class inherited form that class
+
+
+// ----------------------------------------------------------------------------------------------------------------------------
+// Inheritance
+
+class subUser extends User{
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+// protected properties are accessible within the class as well as class which is inherited from it
+// but is still not by the object that created from that class thats the little deference in between protected and private
