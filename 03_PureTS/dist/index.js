86df06f141e217861a7f7946335e291772d09d6e
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-15 22:31:44 +0530

    More in classes

--- 03_PureTS/dist/index.js
+++ 03_PureTS/dist/index.js
@@ -2,8 +2,22 @@
 class User {
     constructor(name, email) {
         this.city = "";
+        this.pinCode = 400001;
+        this.phoneNo = 123455678;
         this.name = name;
         this.email = email;
     }
 }
+// some professionals write class code like below it produces same code as above
+// it is just a syntax difference you can go for any one
+class User2 {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.pinCode = 400001;
+    }
+}
 const jack = new User("Jack Sharma", "jack@gmail.com");
+jack.city = "mumbai";
+// jack.pinCode = 412330    // restricted as it is read only
+// jack.phoneNo         // not accessible as it is private means it is accessible within class
--- 03_PureTS/src/index.ts
+++ 03_PureTS/src/index.ts
@@ -2,6 +2,8 @@ class User{
     name :string
     email: string
     city: string = ""
+    readonly pinCode: number = 400001
+    private phoneNo: number = 123455678
 
     constructor(name: string, email: string){
         this.name = name
@@ -9,5 +11,18 @@ class User{
     }
 }
 
+// some professionals write class code like below it produces same code as above
+// it is just a syntax difference you can go for any one
+class User2{
+    readonly pinCode: number = 400001
+    constructor(
+        public name: string,
+        public email: string
+    ){
+    }
+}
+
 const jack = new User("Jack Sharma", "jack@gmail.com");
 jack.city = "mumbai"
+// jack.pinCode = 412330    // restricted as it is read only
+// jack.phoneNo         // not accessible as it is private means it is accessible within class
