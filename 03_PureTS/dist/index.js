a34d8f2e2d9a4043f967ed8fe29b22ac75611884
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-15 22:48:20 +0530

    getters, setter, private methods in typescript

--- 03_PureTS/dist/index.js
+++ 03_PureTS/dist/index.js
@@ -4,19 +4,41 @@ class User {
         this.city = "";
         this.pinCode = 400001;
         this.phoneNo = 123455678;
+        this._courseCount = 1;
         this.name = name;
         this.email = email;
+    }
+    // private method
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    // getter method
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    // getter method
+    get courseCount() {
+        return this._courseCount;
+    }
+    // setter methods
+    // setter method does not have any return type they are meant or setting values
+    set couseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 // some professionals write class code like below it produces same code as above
 // it is just a syntax difference you can go for any one
-class User2 {
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-        this.pinCode = 400001;
-    }
-}
+// class User2{
+//     readonly pinCode: number = 400001
+//     constructor(
+//         public name: string,
+//         public email: string
+//     ){
+//     }
+// }
 const jack = new User("Jack Sharma", "jack@gmail.com");
 jack.city = "mumbai";
 // jack.pinCode = 412330    // restricted as it is read only
--- 03_PureTS/src/index.ts
+++ 03_PureTS/src/index.ts
@@ -4,23 +4,49 @@ class User{
     city: string = ""
     readonly pinCode: number = 400001
     private phoneNo: number = 123455678
+    private _courseCount: number = 1
 
     constructor(name: string, email: string){
         this.name = name
         this.email = email
     }
+
+    // private method
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+
+    // getter method
+    get getAppleEmail(): string {
+        return `apple ${this.email}`
+    }
+
+    // getter method
+    get courseCount(): number{
+        return this._courseCount
+    }
+
+    // setter methods
+    // setter method does not have any return type they are meant or setting values
+    set couseCount(courseNum: number){
+        if (courseNum <=1) {
+            throw new Error("Course count should be more than 1")
+        }
+
+        this._courseCount = courseNum
+    }
 }
 
 // some professionals write class code like below it produces same code as above
 // it is just a syntax difference you can go for any one
-class User2{
-    readonly pinCode: number = 400001
-    constructor(
-        public name: string,
-        public email: string
-    ){
-    }
-}
+// class User2{
+//     readonly pinCode: number = 400001
+//     constructor(
+//         public name: string,
+//         public email: string
+//     ){
+//     }
+// }
 
 const jack = new User("Jack Sharma", "jack@gmail.com");
 jack.city = "mumbai"
