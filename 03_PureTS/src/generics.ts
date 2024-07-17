afde63aad8b2f83e21b0f84b68d80cd9417c5fc4
Ansari Anas <111463267+ansarianas23@users.noreply.github.com>
2024-07-17 18:51:41 +0530

    more in generic

--- 03_PureTS/src/generics.ts
+++ 03_PureTS/src/generics.ts
@@ -41,3 +41,61 @@ identityFour<bottle>(
         brand: "Milton"
     }
 )
+
+// ----------------------------------------------------------------------------
+// functions Generics
+
+function getSearchProducts<T>(products: T[]): T{
+    // do some database operations
+    const myIndex = 5
+    return products[myIndex]
+}
+
+const getMoreSearchProducts = <HX,>(products: HX[]): HX=>{
+    // do some database operations
+    const myIndex = 5
+    return products[myIndex]
+}
+
+// <HX,> in above code indicates thats not an JSX syntax but rather a generic type
+// it can be seen a lot of codebase specially with react code
+
+interface Database {
+    connecton: string,
+    username: string,
+    password: string
+}
+
+function anotherFunction<T,U extends Database>(val1: T, val2: U):object{
+    return{
+        val1,val2
+    }
+}
+
+anotherFunction(2, {
+    connecton: "striasdng",
+    username: "strasdasing",
+    password: "asd"
+})
+
+// -----------------------------------------------------------------------------------
+// classes with generics
+
+interface Quiz{
+    name: string
+    type: string
+}
+
+interface Course{
+    name:string
+    author: string
+    subject: string
+}
+
+class sellable<T>{
+    public cart: T[] = []
+
+    addToCart(product: T){
+        this.cart.push(product)
+    }
+}
